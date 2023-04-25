8787beead372f5fb47a734181ee8191d4bfbe02b
hyejin-bea <asha72758@gmail.com>
2023-04-25 13:06:14 +0900

    first commit

---
+++ src/main/java/com/example/goodNeighbor5/GoodNeighbor5Application.java
@@ -0,0 +1,13 @@
+package com.example.goodNeighbor5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodNeighbor5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodNeighbor5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/goodNeighbor5/GoodNeighbor5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goodNeighbor5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodNeighbor5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
